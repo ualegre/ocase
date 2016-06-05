c584235f12a4b43fb048c8a79dc37096f3cbd287
Unai Alegre <u.alegre@mdx.ac.uk>
2016-06-05 19:33:50 +0100

    Updated to Modelio 3.5

--- ocase/src/main/java/edu/casetools/ocase/module/impl/OCaseModule.java
+++ ocase/src/main/java/edu/casetools/ocase/module/impl/OCaseModule.java
@@ -1,46 +1,24 @@
-/*
- * Copyright 2015 @author Unai Alegre 
- * 
- * This file is part of OCASE (Ontologies for Context-Aware Systems Engineering), a module 
- * of Modelio that helps the automatic generation of ontologies based on the Situational 
- * Parameters gathered during the requirements elicitation phase of a Context-Aware System (C-AS),
- *  using RCASE (Requirements for Context-Aware Systems Engineering) tool. 
- * 
- * OCASE is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * OCASE is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with OCASE.  If not, see <http://www.gnu.org/licenses/>.
- * 
- */
 package edu.casetools.ocase.module.impl;
 
-import org.modelio.api.model.IModelingSession;
 import org.modelio.api.module.AbstractJavaModule;
-import org.modelio.api.module.IModuleAPIConfiguration;
-import org.modelio.api.module.IModuleSession;
-import org.modelio.api.module.IModuleUserConfiguration;
-import org.modelio.api.module.IParameterEditionModel;
-import org.modelio.metamodel.mda.ModuleComponent;
+import org.modelio.api.module.context.IModuleContext;
+import org.modelio.api.module.lifecycle.IModuleLifeCycleHandler;
+import org.modelio.api.module.parameter.IParameterEditionModel;
+
+import edu.casetools.ocase.module.api.OCaseResources;
 
 /**
  * Implementation of the IModule interface. <br>
  * All Modelio java modules should inherit from this class.
  * 
  */
-@SuppressWarnings("all")
 public class OCaseModule extends AbstractJavaModule {
 
     private OCasePeerModule peerModule = null;
 
     private OCaseSession session = null;
+
+    private static OCaseModule instance;
 
     /**
      * Builds a new module.
@@ -49,22 +27,18 @@ public class OCaseModule extends AbstractJavaModule {
      * This constructor must not be called by the user. It is automatically
      * invoked by Modelio when the module is installed, selected or started.
      * 
-     * @param modelingSession
-     *            the modeling session this module is deployed into.
-     * @param model
-     *            the model part of this module.
-     * @param moduleConfiguration
-     *            the module configuration, to get and set parameter values from
-     *            the module itself.
-     * @param peerConfiguration
-     *            the peer module configuration, to get and set parameter values
-     *            from another module.
+     * @param moduleContext
+     *            context of the module, needed to access Modelio features.
      */
-    public OCaseModule(IModelingSession modelingSession, ModuleComponent moduleComponent,
-	    IModuleUserConfiguration moduleConfiguration, IModuleAPIConfiguration peerConfiguration) {
-	super(modelingSession, moduleComponent, moduleConfiguration);
+    public OCaseModule(IModuleContext moduleContext) {
+	super(moduleContext);
 	this.session = new OCaseSession(this);
-	this.peerModule = new OCasePeerModule(this, peerConfiguration);
+	this.peerModule = new OCasePeerModule(this, moduleContext.getPeerConfiguration());
+	instance = this;
+    }
+
+    public OCaseModule getInstance() {
+	return instance;
     }
 
     @Override
@@ -73,14 +47,14 @@ public class OCaseModule extends AbstractJavaModule {
     }
 
     /**
-     * Return the session attached to the current module.
+     * Return the lifecycle handler attached to the current module.
      * <p>
      * <p>
-     * This session is used to manage the module lifecycle by declaring the
+     * This handler is used to manage the module lifecycle by declaring the
      * desired implementation on start, select... methods.
      */
     @Override
-    public IModuleSession getSession() {
+    public IModuleLifeCycleHandler getLifeCycleHandler() {
 	return this.session;
     }
 
@@ -97,7 +71,37 @@ public class OCaseModule extends AbstractJavaModule {
 
     @Override
     public String getModuleImagePath() {
-	return "/res/icons/module_16.png";
+	return OCaseResources.ICON_MODULE;
     }
 
+    @Override
+    public int hashCode() {
+	final int prime = 31;
+	int result = super.hashCode();
+	result = prime * result + ((peerModule == null) ? 0 : peerModule.hashCode());
+	result = prime * result + ((session == null) ? 0 : session.hashCode());
+	return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+	if (this == obj)
+	    return true;
+	if (!super.equals(obj))
+	    return false;
+	if (getClass() != obj.getClass())
+	    return false;
+	OCaseModule other = (OCaseModule) obj;
+	if (peerModule == null) {
+	    if (other.peerModule != null)
+		return false;
+	} else if (!peerModule.equals(other.peerModule))
+	    return false;
+	if (session == null) {
+	    if (other.session != null)
+		return false;
+	} else if (!session.equals(other.session))
+	    return false;
+	return true;
+    }
 }
